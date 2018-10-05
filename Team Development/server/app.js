c8b830464b727f164d27137b249abb3366722cd2
Trace Larson <trace.larson4917@gmail.com>
2018-10-04 19:39:57 -0500

    created app.js and added database connection and configured bodyparser and routes

---
+++ Team Development/server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+
+// Configure Body Parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Connect to MongoDB Database
+mongoose.connect(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}`)
+
+// Get default connection
+var db = mongoose.connection
+
+// Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
+// Bind connection to connection event
+db.once('open', () => console.log('DATABASE CONNECTED SUCCESSFULLY'))
+
+// ROUTES
+// TODO: add routes
+const index = require('./routes/index')
+
+app.use('/', index)
+
+
+module.exports = app
