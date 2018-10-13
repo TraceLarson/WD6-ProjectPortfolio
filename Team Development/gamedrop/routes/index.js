c4e4409749d907428a1c9d798d94871701d6be53
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 16:18:24 -0700

    Make use of callback in find call to ensure correct num of records are found before rendering view

--- Team Development/gamedrop/routes/index.js
+++ Team Development/gamedrop/routes/index.js
@@ -6,11 +6,13 @@ const Product = require("../models/product");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Find all products in db
-  let products = Product.find();
+  let products = Product.find((err, docs) => {
+    // Use callback to ensure all records are found prior to passing data and rendering view
 
-  // Render products index view
-  // Pass products data to view
-  res.render('shop/index', { title: 'GameDrop', products: products});
+    // Render products index view
+    // Pass products data to view
+    res.render('shop/index', { title: 'GameDrop', products: docs});
+  });
 });
 
 module.exports = router;
