6b52e29b3f6327e2ccd7096dd6451d15eafd7f63
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 08:11:52 -0700

    Create seed dir and begin to create a seeder file to fill db with products

---
+++ Team Development/gamedrop/seed/product-seeder.js
@@ -0,0 +1,8 @@
+const Product = require("../models/product");
+
+let product = new Product({
+    imgPath: "images/game-assassins-creed-odyssey.jpg",
+    title: "Assassins Creed Odyssey",
+    description: "An awesome, enthralling game not for the feeble.",
+    price: 11.99
+});
