6ed80726ce9b9138fc413f95933b0f70957a7b6f
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 16:24:20 -0700

    Slice returned docs into chunks and add them to an arr

--- Team Development/gamedrop/routes/index.js
+++ Team Development/gamedrop/routes/index.js
@@ -9,6 +9,14 @@ router.get('/', function(req, res, next) {
   let products = Product.find((err, docs) => {
     // Use callback to ensure all records are found prior to passing data and rendering view
 
+    let productChunks = []; // Array to hold groups (chunks) of returned product docs
+    let chunkSize = 3; // Number of product docs in each group
+
+    for (let i = 0; i < docs.length; i += chunkSize) { // Loop through returned documents, incrementing by chunkSize each iteration
+      // Push a chunk of product docs into arr
+      productChunks.push(docs.slice(i, i+chunkSize)); // Slice from current arr position to chunkSize positions forward
+    }
+
     // Render products index view
     // Pass products data to view
     res.render('shop/index', { title: 'GameDrop', products: docs});
