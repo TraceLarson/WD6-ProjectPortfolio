9e953549f85f3b9ff05c85b7cd71902f4336309c
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 16:11:46 -0700

    Import product model so data can be fetched

--- Team Development/gamedrop/routes/index.js
+++ Team Development/gamedrop/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const Product = require("../models/product");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('shop/index', { title: 'Express' });
