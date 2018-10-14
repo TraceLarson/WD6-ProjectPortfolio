e952746dd00054d19b288eccae362fc130e587f9
Trace Larson <trace.larson4917@gmail.com>
2018-10-14 14:40:24 -0500

    added Route for user-reviews in src/index.js

---
+++ Team Development/client/src/components/UserReviews.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class UserReviews extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+UserReviews.propTypes = {};
+
+export default UserReviews;
--- Team Development/client/src/index.js
+++ Team Development/client/src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Header from './components/header';
 import Show from './components/show';
 import Footer from './components/footer';
+import UserReviews from "./components/UserReviews";
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Header />
-      <Route exact path='/' component={App} />
-      <Route path='/show/:id' component={Show} />
-      <Footer />
-    </div>
-  </Router>,
-  document.getElementById('root')
+    <Router>
+        <div>
+            <Header/>
+            <Route exact path='/' component={App}/>
+            <Route path='/show/:id' component={Show}/>
+            <Route path={'/reviews'} component={UserReviews}/>
+            <Footer/>
+        </div>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
