ca94f14826b63021a712742b6c1cd178725d9a0e
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 21:39:44 -0700

    Remove boilerplate users route file and references to it

--- Team Development/gamedrop/app.js
+++ Team Development/gamedrop/app.js
@@ -13,7 +13,6 @@ const validator = require("express-validator");
 
 /* Routes */
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 /* Express App */
 let app = express();
@@ -48,7 +47,6 @@ app.use(express.static(path.join(__dirname, 'public'))); // Static file serving
 
 /* Routes */
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
