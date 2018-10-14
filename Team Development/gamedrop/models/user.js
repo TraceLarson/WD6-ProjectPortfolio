c5c2f4783f2b6f0ee4a74e8d7255f2c8ac8c3524
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 19:26:15 -0700

    Add method to encrypt passwords

--- Team Development/gamedrop/models/user.js
+++ Team Development/gamedrop/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs")
+
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
@@ -11,5 +13,12 @@ let userSchema = new Schema({
         required: true
     }
 });
+
+// Create a method to encrypt a password
+userSchema.methods.encryptPassword = function(password) {
+    // Hash and salt the password
+    // Return result
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
 
 module.exports = mongoose.model("User", userSchema);
