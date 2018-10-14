17dabcab0cfa8e69b161dd21e3f2b71adcacd785
Daniel Hughes <hughesd22@gmail.com>
2018-10-13 19:18:38 -0700

    Define deserializeUser func

--- Team Development/gamedrop/config/passport.js
+++ Team Development/gamedrop/config/passport.js
@@ -2,3 +2,16 @@ const passport = require("passport");
 const User = require("../models/user");
 const localStrategy = require("passport-local").Strategy;
 
+// Tells passport how to store the user in the session
+passport.serializeUser((user, done) => {
+    // Serialize user by id
+    done(null, user.id);
+});
+
+// Tells passport how to find the user using session information
+passport.deserializeUser((id, done) => {
+    // Find user in database
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
