373fe3585655b75c4675271cc6080a1590743c31
Trace Larson <trace.larson4917@gmail.com>
2018-10-14 16:10:19 -0500

    removed modal

--- Team Development/client/src/components/UserReviews.js
+++ Team Development/client/src/components/UserReviews.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react';
-import {
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter
-} from 'reactstrap'
+
 // import PropTypes from 'prop-types';
 import Review from "./Review";
 
@@ -50,17 +45,6 @@ class UserReviews extends Component {
         })
         return (
             <div>
-                <button className={'btn btn-primary'} onClick={this.toggle} >Add Review</button>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-                    <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </ModalBody>
-                    <ModalFooter>
-                        <button className="btn btn-primary" onClick={this.toggle}>Do Something</button>{' '}
-                        <button className="btn btn-secondary" onClick={this.toggle}>Cancel</button>
-                    </ModalFooter>
-                </Modal>
                 <div>
                     {reviewList}
                 </div>
