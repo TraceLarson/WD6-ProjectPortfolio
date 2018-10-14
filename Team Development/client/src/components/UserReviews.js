ad207603dae064262aabed68695fe0f403925ea8
Trace Larson <trace.larson4917@gmail.com>
2018-10-14 14:57:24 -0500

    added intial state to UserReviews

--- Team Development/client/src/components/UserReviews.js
+++ Team Development/client/src/components/UserReviews.js
@@ -2,13 +2,36 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class UserReviews extends Component {
-    render() {
-        return (
-            <div>
+    state = {
+        reviews:
+            [
+                {
+                    User: 'Trace@email.com',
+                    Message: 'This is the message I wanted to leave'
+                },
+                {
+                    User: 'Chris@email.com',
+                    Message: 'This is the message I wanted to leave'
+                },
+                {
+                    User: 'Dan@email.com',
+                    Message: 'This is the message I wanted to leave'
+                },
 
-            </div>
-        );
+            ]
     }
+
+
+
+render()
+{
+
+    return (
+        <div>
+            <h1>User Reviews</h1>
+        </div>
+    );
+}
 }
 
 UserReviews.propTypes = {};
