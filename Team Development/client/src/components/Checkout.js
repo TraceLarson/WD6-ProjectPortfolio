2d6de6838fa8e650c7092a90e9a855fdfc37aa78
Trace Larson <trace.larson4917@gmail.com>
2018-10-18 01:09:04 -0500

    completed basic form inputs with SOME styling

--- Team Development/client/src/components/Checkout.js
+++ Team Development/client/src/components/Checkout.js
@@ -4,35 +4,120 @@ import PropTypes from 'prop-types';
 class Checkout extends Component {
 	render() {
 		return (
-			<div>
-				<div className="container">
-					<form>
-						<div className="form-group row">
-							<label for="inputName" className="col-sm-1-12 col-form-label"></label>
-							<div className="col-sm-1-12">
-								<input
-									type="text|password|email|number|submit|date|datetime|datetime-local|month|color|range|search|tel|time|url|week"
-									className="form-control" name="inputName" id="inputName" placeholder=""/>
-							</div>
-						</div>
-						<fieldset className="form-group row">
-							<legend className="col-form-legend col-sm-1-12">Group name</legend>
-							<div className="col-sm-1-12">
+			<div className={'row'}>
+				<div className={'col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3'}>
+					<h1>Checkout</h1>
+					<h4>Your Total: $</h4>
+					<form action="/" action={'POST'} id={'checkout-form'} className={'ml-5'}>
+						<div className={'row'}>
 
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="name">Name</label>
+									<input type="text"
+									       name="name"
+									       id="name"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">Your full name.</small>
+								</div>
 							</div>
-						</fieldset>
-						<div className="form-group row">
-							<div className="offset-sm-2 col-sm-10">
-								<button type="submit" className="btn btn-primary">Action</button>
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="address">Address</label>
+									<input type="text"
+									       name="address"
+									       id="address"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">Billing address associated with this card</small>
+								</div>
 							</div>
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="card-name">Card Holder Name</label>
+									<input type="text"
+									       name="card-name"
+									       id="card-name"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">Name as it appears on your credit card</small>
+								</div>
+							</div>
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="card-number">Credit Card Number</label>
+									<input type="text"
+									       name="card-number"
+									       id="card-number"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">16 digit number on front of card</small>
+								</div>
+							</div>
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="card-expiry-month">Expiration Month</label>
+									<input type="text"
+									       name="card-expiry-month"
+									       id="card-expiry-month"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">MM</small>
+								</div>
+							</div>
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="card-expiry-year">Expiration Year</label>
+									<input type="text"
+									       name="card-expiry-year"
+									       id="card-expiry-year"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">YY</small>
+								</div>
+							</div>
+
+							<div className={'col-xs-12'}>
+								<div className="form-group">
+									<label className={'badge'} htmlFor="card-cvc">CVC</label>
+									<input type="text"
+									       name="card-cvc"
+									       id="card-cvc"
+									       className="form-control"
+									       aria-describedby="helpId"
+									       required={true}
+									/>
+									<small id="helpId" className="text-muted">3 Digit code on back of card</small>
+								</div>
+							</div>
+
 						</div>
 					</form>
 				</div>
 			</div>
-	);
+		)
 	}
-	}
+}
 
-	Checkout.propTypes = {};
+Checkout.propTypes = {};
 
-	export default Checkout;
+export default Checkout;
